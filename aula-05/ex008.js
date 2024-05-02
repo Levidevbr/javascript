932832fd146bd462031b1abbde4a374fe4667b52
Leonardo Vinicius <152725239+Levidevbr@users.noreply.github.com>
2024-05-02 11:13:16 -0300

    novas apreendizagem

---
+++ aula-05/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80 
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitido. MULTADO!`)
+}
+console.log(`Dirija sempre utilizando cinto de seguranca!`)
